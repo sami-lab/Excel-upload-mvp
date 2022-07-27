919d7f8a3a21f362dd9e13ad96ccbef05765d6a1
Sami <s.m.sami125@gmail.com>
2022-07-27 20:14:17 +0500

    backend

---
+++ middleware/uploadServer.js
@@ -0,0 +1,56 @@
+var multer = require('multer');
+const AppError = require('../utils/appError');
+
+var fs = require('fs');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    if (fs.existsSync('/public/files')) {
+      callback(null, 'public/files');
+    } else {
+      fs.mkdir('/public/files', { recursive: true }, () => {
+        callback(null, 'public/files');
+      });
+    }
+  },
+  filename: (req, file, callback) => {
+    //const ext = file.mimetype.split('/')[1]
+    callback(null, `${Date.now()}-${file.originalname}`);
+    //callback(null,`user-${req.user.id}-${Data.now()}-${file.filename}.${ext}`)
+  },
+});
+
+const multerFilter = (req, file, cb) => {
+  let allowedType = '';
+  switch (file.fieldname) {
+    case 'images':
+      allowedType = /\.(jpg|JPG|jpeg|JPEG|png|PNG|webp)$/;
+      break;
+
+    case 'video':
+      allowedType = /\.(wmv|avi|mov|3gp|mp4|flv)$/;
+      break;
+    case 'attachement':
+      allowedType = /\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|pdf|doc|docx)$/;
+      break;
+    case 'file':
+      allowedType = /\.(xlsx)$/;
+      break;
+    default:
+      allowedType = /\.(jpg|JPG|jpeg|JPEG|png|PNG)$/;
+      break;
+  }
+  if (file.originalname.match(allowedType)) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Invalid Format testing', 400), false);
+  }
+};
+module.exports = multer({
+  storage: storage,
+  fileFilter: multerFilter,
+  onError: function (err, next) {
+    next(next(new AppError('Error in Updloading Image.', 500)));
+  },
+});
--- routes/filesRoutes.js
+++ routes/filesRoutes.js
@@ -2,11 +2,14 @@ const express = require('express');
 const filesController = require('../controllers/filesController');
 
 //const protect = require('../middleware/protect');
-
+const uploadServer = require('../middleware/uploadServer');
 const router = express.Router();
-router.use(protect);
+//router.use(protect);
 
-router.route('/').post(filesController.createOne).get(filesController.getFiles);
+router
+  .route('/')
+  .post(uploadServer.single('file'), filesController.createOne)
+  .get(filesController.getFiles);
 
 router.route('/:id').get(filesController.getFile).delete(filesController.delete);
 
