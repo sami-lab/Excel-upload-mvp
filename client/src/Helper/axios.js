938772097de1d3a97528c1c2050f33da35612572
Sami <s.m.sami125@gmail.com>
2022-07-30 23:00:00 +0500

    console off

--- client/src/Helper/axios.js
+++ client/src/Helper/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-let baseURL = process.env.REACT_APP_API_URL + '/api/v1';
+//let baseURL = process.env.REACT_APP_API_URL + '/api/v1';
+let baseURL = '/api/v1';
 
 const instance = axios.create({
   baseURL,
